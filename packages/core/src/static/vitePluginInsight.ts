e4861ffb19610ad70bcad31ea8f3345beff8aaad
xiyueyezibile <2859439384@qq.com>
2025-01-25 23:16:14 +0800

    feat: collect external modules and css modules

--- packages/core/src/static/vitePluginInsight.ts
+++ packages/core/src/static/vitePluginInsight.ts
@@ -1,5 +1,5 @@
 
-import { existsSync, writeFileSync } from "fs";
+import { existsSync, readFileSync, writeFileSync } from "fs";
 import path from 'path'
 
 
@@ -7,12 +7,26 @@ import path from 'path'
 interface Config {
   root: string;
   entry?: string;
+  pkgPath?: string;
 }
 
-const externals = ["node_modules", "css", "sass", "less"]
+const externals = ['node_modules']
 
 function idInExternals(id: string) {
-  return externals.some((external) => id.includes(external))
+    
+  return externals.some((external) => {
+    // if(id.includes(external)) console.log(id);
+    
+    return id.includes(external)
+  })
+}
+
+function getExternalName(id: string) {
+    if(!idInExternals(id)) return id
+    const arr = id.split(path.sep)
+    const i = arr.findIndex((item) => item === 'node_modules')
+    const name = arr[i + 1] === '.pnpm'? arr[i + 2] : arr[i + 1]
+    return name
 }
 
 function logCircleModules(circleModules: Module[]) {
@@ -151,18 +165,18 @@ class ModuleGraph {
     if(this._moduleIds.has(entryId)) {
       const newValue = this._moduleIds.get(entryId) + 1
       this._moduleIds.set(entryId, newValue)
-      id = `${entryId}-${newValue}`
+      id = `${newValue}`
     } else {
       this._moduleIds.set(entryId, 1)
-      id = `${entryId}-1`
+      id = `1`
     }
     const nameArr = entryId.split(path.sep)
     const tree: ModuleTree = {
-        parentId: parent? parent.id : undefined,
+        parentId: parent? `${parent.pathId}-${parent.id}` : undefined,
         id: id,
         pathId: entryId,
         depth: parent? parent.depth + 1 : 0,
-        name: nameArr.slice(nameArr.length >= 2? nameArr.length - 2: 0).join('/'),
+        name: nameArr.slice(nameArr.length >= 2? nameArr.length - 2: 0).join(path.sep),
         children: [],
         idpath: parent? [...parent.idpath, id] : [id],
         // circleIds: [],
@@ -175,15 +189,18 @@ class ModuleGraph {
     if(new Set(tree.path).size !== tree.path.length) {
       return tree
     }
+    
     if(this.graph.has(entryId)) {
+        
         const rootModule = this.graph.get(entryId);
+        
 
         // 遍历导入模块
         tree.children = rootModule.importedIds.map(module => {
             let kid: ModuleTree | null = null
             kid = this.transform(module.id, depth - 1, tree)
             if(kid) {
-                kid.parentId = tree.id
+                kid.parentId = `${tree.pathId}-${tree.id}`
                 kid.depth = tree.depth + 1
                 kid.path = [...tree.path, kid.pathId]
                 kid.idpath = [...tree.idpath, kid.id]
@@ -216,15 +233,30 @@ class Bundle {
   /** 加载模块 */
   loadModules = new Map<string, any>();
   noBundleModules = new Map<string, any>();
-  
+  /** 外部模块 */
+  externalModules = new Map<string, any>()
+  /** 实际外部模块 */
+  originExternalModules = new Map<string, any>()
+  unUsedExternalModules = new Map<string, any>()
+  options: Config;
+  externals = []
   constructor(options: Config) {
+    this.options = options
+    if(options.pkgPath) {
+        const pkg = readFileSync(options.pkgPath, 'utf-8')
+        const pkgJson = JSON.parse(pkg)
+        
+        this.externals = [...(Object.keys(pkgJson.dependencies || [])), ...(Object.keys(pkgJson.devDependencies || []))]
+    }
     const jsonName = 'moduleTree.json'
     const jsonPath = path.join(options.root,jsonName)
     if(!existsSync(jsonPath)) writeFileSync(jsonPath, JSON.stringify([], null, 2));
   }
+  
+
   /** 获取实际被打包的模块 */
-  resolveOriginModuleByBundle(fn: (originModule: Map<string, any>) => void) {
-    fn(this.originModules)
+  resolveOriginModuleByBundle(fn: (originModules: Map<string, any>, originExternalModules: Map<string, any>, externals: string[]) => void) {
+    fn(this.originModules, this.originExternalModules, this.externals)
   }
   /** 获取编译阶段模块 */
   resolveLoadModule(id: string) {
@@ -232,12 +264,16 @@ class Bundle {
     if (!idInExternals(id)) {
         
       this.loadModules.set(id, id)
-        
+    } else {
+        const externalArr = getExternalName(id).split('@')
+        const externalName = externalArr.length === 3? externalArr[0] + '@' + externalArr[1]: externalArr[0]
+        if(this.externals.includes(externalName)) this.externalModules.set(getExternalName(id), getExternalName(id))
     }
   }
   /** 寻找在编译模块和实际模块都存在的模块 */
   findLoadModuleWithOriginModule() {
     const result = {};
+    const externalResult = {}
     
     this.loadModules.forEach((module,id) => {
         
@@ -247,11 +283,23 @@ class Bundle {
             this.noBundleModules.set(id, module);
         }
     })
-    return result;
+    this.externalModules.forEach((module,id) => {
+        if(this.originExternalModules.has(id)) {
+            externalResult[id] = this.originExternalModules.get(id)
+        } else {
+            this.unUsedExternalModules.set(id, module);
+        }
+    })
+    console.log(Object.keys(externalResult));
+    
+    return {
+        modules: result,
+        externalModules: externalResult
+    };
   }
   /** 根据map生产moduleGraph */
   newModuleByMap(map: Record<string, any>) {
-    this.moduleGraph = new ModuleGraph(this,map);
+    this.moduleGraph = new ModuleGraph(this,map.modules);
   }
 
 }
@@ -269,16 +317,20 @@ export function vitePluginInsight(options: Config): any{
       globleBundle.resolveLoadModule(id);
     },
     generateBundle(_, bundle) {
-        console.log('执行了');
         
       // 根据bundle获取实际被打包的模块
-      globleBundle.resolveOriginModuleByBundle((originModules) => {
+      globleBundle.resolveOriginModuleByBundle((originModules, originExternalModules, externals) => {
         const distLists = Object.values(bundle);
         distLists.forEach((dist) => {
           Object.entries(dist["modules"] || {}).forEach(([id, data]) => {
             if(!idInExternals(id)) {
               originModules.set(id, data)
               
+            } else {
+                
+                const externalArr = getExternalName(id).split('@')
+                const externalName = externalArr.length === 3? externalArr[0] + '@' + externalArr[1]: externalArr[0]
+                if(externals.includes(externalName)) originExternalModules.set(getExternalName(id), [...(originExternalModules.get(getExternalName(id)) || []), data])
             }
             
           });
@@ -291,12 +343,11 @@ export function vitePluginInsight(options: Config): any{
       const moduleGraph = globleBundle.moduleGraph;
       moduleGraph.rootId = options.root;
       moduleGraph.entryId = options.entry;
-        // 获取所以导入导出关系
-      Object.keys(map).forEach((id) => {
+        // 获取所所有导入导出关系
+      Object.keys(map.modules).forEach((id) => {
         const info = this.getModuleInfo(id);
-        
-        
         if(info && info.isIncluded) {
+            
             moduleGraph.importers.set(info.id, info.importers)
             moduleGraph.importedIds.set(info.id, info.importedIds)
             moduleGraph.dynamicImporters.set(info.id, info.dynamicImporters)
